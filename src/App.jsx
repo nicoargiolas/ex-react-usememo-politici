f663769a33c061efc47f15362c4f253aa06aec57
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-17 15:34:53 +0200

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+// 💡 Premessa: Stai costruendo una pagina per visualizzare una lista di politici. Tuttavia, vuoi evitare calcoli inutili e ottimizzare la performance del tuo componente. Segui le milestone per migliorare progressivamente il codice
+
+// 📌 Milestone 1: Recuperare e visualizzare i dati
+
+// Effettua una chiamata API a
+// https://boolean-spec-frontend.vercel.app/freetestapi/politicians
+// Salva la risposta in uno stato React (useState).
+// Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
+// Nome (name)
+// Immagine (image)
+// Posizione (position)
+// Breve biografia (biography)
+
+// Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
+
+const PoliticianCard = ({ name, image, position, biography }) => {
+  return (
+    <div className='politician-card'>
+      <h3 className='politician-name'> {name} </h3>
+      <img src={image} alt={name} />
+      <h5 className='politician-position'> {position} </h5>
+      <p className='biography'> {biography} </p>
+    </div>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+
+    const response = await fetch('http://localhost:3333/politicians');
+    const data = await response.json();
+
+    setUsers(data);
+  }
+  useEffect(() => { loadUsers() }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {users.map((u, i) => (
+        <PoliticianCard key={i} {...u} />
+      ))}
     </>
   )
 }

e282f17aa7786453de50451c7568fce1cf5aa74b
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-17 15:59:49 +0200

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import './App.css'
 
-// 💡 Premessa: Stai costruendo una pagina per visualizzare una lista di politici. Tuttavia, vuoi evitare calcoli inutili e ottimizzare la performance del tuo componente. Segui le milestone per migliorare progressivamente il codice
+// 📌 Milestone 2: Implementare la ricerca ottimizzata
 
-// 📌 Milestone 1: Recuperare e visualizzare i dati
+// Aggiungi un campo di ricerca (<input type="text">) sopra la lista dei politici.
+// Permetti all’utente di filtrare i risultati in base a nome o biografia (se il testo cercato è incluso). Suggerimento: Creare un array derivato filtrato, che viene aggiornato solo quando cambia la lista di politici o il valore della ricerca.
+// ❌ Non usare useEffect per aggiornare l’array filtrato.
 
-// Effettua una chiamata API a
-// https://boolean-spec-frontend.vercel.app/freetestapi/politicians
-// Salva la risposta in uno stato React (useState).
-// Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
-// Nome (name)
-// Immagine (image)
-// Posizione (position)
-// Breve biografia (biography)
-
-// Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
+// Obiettivo: Migliorare le prestazioni evitando ricalcoli inutili quando il valore della ricerca non cambia.
 
 const PoliticianCard = ({ name, image, position, biography }) => {
   return (
@@ -31,6 +24,7 @@ const PoliticianCard = ({ name, image, position, biography }) => {
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState('');
 
   const loadUsers = async () => {
 
@@ -41,9 +35,17 @@ function App() {
   }
   useEffect(() => { loadUsers() }, []);
 
+  const filteredUsers = useMemo(() => {
+    return users.filter(u =>
+      u.name.toLowerCase().includes(query.toLowerCase()) ||
+      u.biography.toLowerCase().includes(query.toLowerCase())
+    )
+  }, [users, query])
+
   return (
     <>
-      {users.map((u, i) => (
+      <input className='searchbar' type='text' value={query} onChange={(e) => setQuery(e.target.value)} />
+      {filteredUsers.map((u, i) => (
         <PoliticianCard key={i} {...u} />
       ))}
     </>
